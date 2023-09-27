312922d0cabaa4fb7914848db556de3ccea5b068
Vladimir <supervova0505@gmail.com>
2023-09-27 17:41:08 +0300

    Forward list files

---
+++ Forward_list.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+
+
+
+
+
+
+template <class T>
+class Forward_list
+{
+
+protected:
+
+	
+	template<class T>
+	class Node
+	{
+
+
+
+	};
+
+	
+
+
+
+};
+
+
+
+#include "Forward_list.inl"
+
