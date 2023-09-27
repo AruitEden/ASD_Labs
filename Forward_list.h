6b156eb1c63f569fdfa7eb9c2e0936d883b148a5
Vladimir <supervova0505@gmail.com>
2023-09-27 21:41:34 +0300

    Node subclass

--- Forward_list.h
+++ Forward_list.h
@@ -13,16 +13,57 @@ class Forward_list
 
 protected:
 
-	
 	template<class T>
 	class Node
 	{
 
+	private:
 
+		T* const m_data;
+		Node<T>* m_next;
+
+
+	public:
+
+		Node(Node<T>* next, const T& data) :
+			m_next(next), m_data(new T(data)) {}
+
+
+
+		~Node()
+		{
+			delete m_data;
+		}
+
+
+
+		bool HasNext() const {
+			return m_next != nullptr ? true : false;
+		}
+
+		Node<T>* GetNext() {
+			return m_next;
+		}
+
+		Node<T> const* GetNext() const {
+			return m_next;
+		}
+
+		void SetNext(Node<T>* next) {
+			m_next = next;
+		}
+
+		T& GetData() {
+			return *m_data;
+		}
+
+		const T& GetData() const {
+			return *m_data;
+		}
 
 	};
-
 	
+
 
 
 
