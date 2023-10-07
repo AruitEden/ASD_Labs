0764165aaf728c9f9f952d570eec9b8e513dd735
Vladimir <supervova0505@gmail.com>
2023-10-07 21:19:02 +0300

    Aliases created

--- Forward_list.h
+++ Forward_list.h
@@ -8,29 +8,37 @@
 
 
 
+
 template <class T>
 class Forward_list
 {
+
+private:
+
+	template<class T, class S> friend class Iterator;
 
 
 protected:
 
 	using List_iterator = Iterator<T, Forward_list<T>>;
 
-	template<class T>
+	using node_T = T;
+
+
+
 	class Node
 	{
 
 	private:
 
-		T* const m_data;
+		node_T* const m_data;
 		Node* m_next;
 
 
 	public:
 
-		Node(Node* const next, const T& data) :
-			m_next(next), m_data(new T(data)) {}
+		Node(Node* const next, const node_T& data) :
+			m_next(next), m_data(new node_T(data)) {}
 
 
 
@@ -41,27 +49,27 @@ protected:
 
 
 
-		bool HasNext() const 
+		bool HasNext() const
 		{
 			return m_next != nullptr ? true : false;
 		}
 
-		Node* GetNext() const 
+		Node* GetNext() const
 		{
 			return m_next;
 		}
 
-		void SetNext(Node* next) 
+		void SetNext(Node* next)
 		{
 			m_next = next;
 		}
 
-		T& GetData() 
+		node_T& GetData()
 		{
 			return *m_data;
 		}
 
-		const T& GetData() const 
+		const node_T& GetData() const
 		{
 			return *m_data;
 		}
@@ -69,115 +77,37 @@ protected:
 	};
 
 
-	Node m_head;
-	
-	template <class T, class S> class Iterator;
 
-	template <class T, class S>
-	friend class Iterator;
 
-	static int a;
-	
 
-	
+
+
 
 };
 
-
-
-template<class T> class A;
-
-template<class T> class B;
-
-template<class T>
-class A 
-{
-
-	template<class T> friend class B;
-
-
-protected:
-
-	class Type
-	{
-		
-	public:
-
-		int _a;
-		Type(int a):_a(a){}
-
-		operator int()
-		{
-			return _a;
-		}
-
-	};
-
-private:
-	static int a;
-
-protected:
-	static int b;
-
-public:
-	static int c;
-
-	void Foo()
-	{
-		
-	}
-
-};
-
-
-template <class T>
-class B 
-{
-
-private:
-	static int a;
-
-protected:
-	static int b;
-
-public:
-	static int c;
-
-	using A_Type = typename A<T>::Type;
-
-	void Foo()
-	{
-		A_Type type = 6;
-		std::cout << type << '\n';
-	}
-	
-
-};
 
 
 
 
 template <class T>
-class Iterator<T, Forward_list<T>> 
+class Iterator<T, Forward_list<T>>
 {
-
-
 
 protected:
 
-	T* m_element;
+	using Node_t = typename Forward_list<T>::Node;
+
+	Node_t* element;
 
 
 public:
 
-	void Foo()
-	{
-		
-	}
+
+
+	
 
 };
 
 
 
 #include "Forward_list.inl"
-
