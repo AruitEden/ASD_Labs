97ce51822ccb63bffb5b82f2435f713ac2894d01
Vladimir <supervova0505@gmail.com>
2023-10-13 21:07:38 +0300

    Stack class, stack traits created

---
+++ Stack.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include "Forward_list.h"
+
+
+
+
+
+template <typename Container>
+struct Stack_traits
+{
+
+};
+
+
+
+
+
+template <class T>
+class Stack
+{
+
+};
