f06d0568269f83c1b3bf9a388db71a247b094b09
Vladimir <supervova0505@gmail.com>
2023-10-13 23:27:33 +0300

    Trait classes made

--- Stack.h
+++ Stack.h
@@ -3,12 +3,21 @@
 #include "Forward_list.h"
 
 
-
-
-
-template <typename Container>
+template <typename T, typename Container>
 struct Stack_traits
 {
+
+	using iterator_type = typename Iterator<T, Container>;
+
+};
+
+
+
+template <typename T>
+struct Stack_traits<T, Forward_list<T>>
+{
+
+	using iterator_type = typename Forward_list_iterator<T>;
 
 };
 
@@ -16,8 +25,29 @@ struct Stack_traits
 
 
 
-template <class T>
+template <class T, class Container = Forward_list<T>>
+class Stack;
+
+
+
+template <typename T, typename Container = Forward_list<T>>
+using Stack_iterator = typename Stack_traits<T, Container>::iterator_type;
+
+
+
+
+template <class T, class Container>
 class Stack
 {
+
+protected:
+
+	Container m_container;
+
+
+public:
+
+	Stack<T, Container>() : m_container(Container()) {}
+
 
 };
