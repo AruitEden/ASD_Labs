f4e82d9588f74d6f7a1c22afae7c24d9e60e1d54
Vladimir <supervova0505@gmail.com>
2023-10-23 01:05:23 +0300

    Final commit, to template get_hash

--- ASD_Lab5.cpp
+++ ASD_Lab5.cpp
@@ -10,8 +10,25 @@ size_t multiplication_hash(size_t key, size_t size, double factor = 0.6180339887
 
 size_t string_hash(const char* key, size_t size);
 
-template <typename K>
-size_t get_hash(K key, size_t size, size_t(*hashing_method)(K, size_t));
+
+
+
+
+enum class Menu_commands
+{
+    DEFAULT = 0,
+    CLEAR_CONSOLE = 'c',
+    QUIT_PROGRAM = 'q',
+    PRINT_HELP = 'h',
+
+    DIVISION_HASH = 1,
+    MULTIPLICATION_HASH = 2,
+    STRING_HASH = 3
+};
+
+void Print_command_info();
+
+
 
 
 
@@ -19,8 +36,104 @@ size_t get_hash(K key, size_t size, size_t(*hashing_method)(K, size_t));
 
 int main()
 {
-    
-    
+
+    size_t num_key, table_size;
+    std::string str_key;
+
+    char command;
+    bool run = true;
+    Print_command_info();
+
+    while (run)
+    {
+
+        std::cout << "----- MAIN MENU -----\n" << "Enter command: ";
+        if (std::cin.peek() == '\n')
+        {
+            std::cin.ignore(1);
+        }
+        std::cin >> command;
+
+        switch ((Menu_commands)command)
+        {
+
+        case Menu_commands::CLEAR_CONSOLE:
+            system("cls");
+            break;
+
+
+        case Menu_commands::PRINT_HELP:
+            Print_command_info();
+            break;
+
+
+        case Menu_commands::QUIT_PROGRAM:
+            run = false;
+            break;
+
+
+        case Menu_commands::DIVISION_HASH:
+            std::cout << "Enter key value: ";
+            std::cin >> num_key;
+            std::cout << "Enter table size: ";
+            std::cin >> table_size;
+
+            std::cout << "Hashing result: " << division_hash(num_key, table_size) << '\n';
+            break;
+
+
+        case Menu_commands::MULTIPLICATION_HASH:
+            std::cout << "Enter key value: ";
+            std::cin >> num_key;
+            std::cout << "Enter table size: ";
+            std::cin >> table_size;
+
+            std::cout << "Hashing result: " << multiplication_hash(num_key, table_size) << '\n';
+            break;
+
+
+        case Menu_commands::STRING_HASH:
+            std::cout << "Enter key value: ";
+            std::getline(std::cin, str_key);
+            std::cout << "Enter table size: ";
+            std::cin >> table_size;
+
+            std::cout << "Hashing result: " << string_hash(str_key.c_str(), table_size) << '\n';
+            break;
+
+
+        default:
+            std::cout << "There is no such command.\n";
+            break;
+
+        }
+
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+void Print_command_info()
+{
+
+    std::cout << "Commands list:\n"
+
+        << "\'" << (char)Menu_commands::PRINT_HELP << "\' - print help\n"
+        << "\'" << (char)Menu_commands::CLEAR_CONSOLE << "\' - clear console\n"
+        << "\'" << (char)Menu_commands::QUIT_PROGRAM << "\' - quit program\n"
+
+        << "\'" << (int)Menu_commands::DIVISION_HASH << "\' - division hash\n"
+        << "\'" << (int)Menu_commands::MULTIPLICATION_HASH << "\' - multiplication hash\n"
+        << "\'" << (int)Menu_commands::STRING_HASH << "\' - string hash\n"
+
+        << "\n\n";
 
 }
 
@@ -48,10 +161,4 @@ size_t string_hash(const char* key, size_t size)
     }
 
     return hash % size;
-}
-
-template<typename K>
-size_t get_hash(K key, size_t size, size_t(*hashing_method)(K, size_t))
-{
-    return hashing_method(key, size);
 }
