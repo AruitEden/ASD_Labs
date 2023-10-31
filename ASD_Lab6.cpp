79ba80c26053f0ae95c26cc0b08c473af9ed31b9
Vladimir <supervova0505@gmail.com>
2023-10-31 09:28:57 +0200

    Menu basics

--- ASD_Lab6.cpp
+++ ASD_Lab6.cpp
@@ -1,10 +1,97 @@
 ﻿#include <iostream>
+#include "Pair.h"
+#include "Hash_table.h"
+#include <string>
+
+
+
+
+
+
+
+enum class Menu_commands
+{
+
+    DEFAULT = 0,
+    CLEAR_CONSOLE = 'c',
+    QUIT_PROGRAM = 'q',
+    PRINT_HELP = 'h'
+
+};
+
+void Print_command_info();
+
+
+
+
 
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    char command;
+    bool run = true;
+    Print_command_info();
+
+    while (run)
+    {
+
+        std::cout << "----- MAIN MENU -----\n" << "Enter command or choose task: ";
+        if (std::cin.peek() == '\n')
+        {
+            std::cin.ignore(1);
+        }
+        std::cin >> command;
+
+        switch ((Menu_commands)command)
+        {
+
+        case Menu_commands::CLEAR_CONSOLE:
+            system("cls");
+            break;
+
+
+        case Menu_commands::PRINT_HELP:
+            Print_command_info();
+            break;
+
+
+        case Menu_commands::QUIT_PROGRAM:
+            run = false;
+            break;
+
+
+
+
+
+        default:
+            std::cout << "There is no such command.\n";
+            break;
+
+        }
+
+    }
+
+    return 0;
+
 }
 
 
+
+
+
+
+
+void Print_command_info()
+{
+
+    std::cout << "Commands list:\n"
+
+        << "\'" << (char)Menu_commands::PRINT_HELP << "\' - print help\n"
+        << "\'" << (char)Menu_commands::CLEAR_CONSOLE << "\' - clear console\n"
+        << "\'" << (char)Menu_commands::QUIT_PROGRAM << "\' - quit program\n"
+
+        << "\n\n";
+
+}
