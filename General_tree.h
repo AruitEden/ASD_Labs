243110d2cf0ff0cf0cd72c57b2176268b3cf82bc
Vladimir <supervova0505@gmail.com>
2023-11-18 23:52:45 +0200

    General_tree class

---
+++ General_tree.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+
+// TODO:
+// 
+// 1) operations:
+// insert
+// erase
+// search
+// 
+// 2) traversals:
+// inorder
+// postorder
+// preorder
+// 
+// 
+//
+
+
+
+
+template <class T>
+class General_tree
+{
+
+
+
+};
+
+
+
+#include "General_tree.inl"
+
