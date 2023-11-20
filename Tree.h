a4bb208bb9e30e17dc8164f882f9881592ee512a
Vladimir <supervova0505@gmail.com>
2023-11-20 19:30:58 +0200

    Insert, min, max

--- Tree.h
+++ Tree.h
@@ -26,11 +26,88 @@ private:
 	Node<T>* root;
 
 
+
+	void insertion(Node<T>*& root, const T& key)
+	{
+
+		if (root == nullptr)
+		{
+			root = new Node<T>(key);
+			return;
+		}
+		if (key > root->key)
+		{
+			insertion(root->right, key);
+		}
+		else if (key < root->key)
+		{
+			insertion(root->left, key);
+		}
+		else
+		{
+			return;
+		}
+
+	}
+
+
 public:
 
 	Tree() : root(nullptr)) {}
 
 	Tree(const T& key) : root(new Node<T>(key)) {}
+
+
+
+	T* min()
+	{
+
+		if (root == nullptr)
+		{
+			return nullptr;
+		}
+
+		Node<T>* current = root;
+
+		while (current->left != nullptr)
+		{
+			current = current->left;
+		}
+
+		return current;
+
+	}
+
+	T* max()
+	{
+
+		if (root == nullptr)
+		{
+			return nullptr;
+		}
+
+		Node<T>* current = root;
+
+		while (current->right != nullptr)
+		{
+			current = current->right;
+		}
+
+		return current;
+
+	}
+
+
+
+	void insert(const T& key)
+	{
+		insertion(root, key);
+	}
+
+	void remove(const T& key)
+	{
+
+	}
 
 };
 
